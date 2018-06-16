35835dc34c0192273abc710cb681b4cb144928cf
Ovidiu Tirla <ovi2022@gmail.com>
2018-06-17 00:46:11 +0300

    custom user session details

---
+++ src/main/java/com/qubiz/server/config/UserDetails.java
@@ -0,0 +1,28 @@
+package com.qubiz.server.config;
+
+/*
+ ******************************
+ # Created by Tirla Ovidiu #
+ # 16.06.2018 #
+ ******************************
+*/
+public class UserDetails {
+    private String clientId;
+    private String email;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
